6d30066d3b4a5c31a63dd92a079dded6e10de5ef
lucas.rocha <lucas.rocha@basis.com.br>
2022-08-18 14:24:09 -0300

    criacao de dominios

---
+++ service/src/main/java/br/com/ifes/videolocadora/service/dominio/Ator.java
@@ -0,0 +1,33 @@
+package br.com.ifes.videolocadora.service.dominio;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Table(name ="tb_ator")
+@Entity
+public class Ator implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_ator")
+	@SequenceGenerator(name = "seq_ator", allocationSize = 1, sequenceName = "seq_ator")
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "excluido")
+	private Boolean excluido;
+
+}
