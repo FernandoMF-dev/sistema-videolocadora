e3ef7aba4cd1c8a87356d697e1675e6d9d7fa998
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2022-09-11 15:39:56 -0300

    Implementando confirmação de exclusão de ator

--- frontend/src/app/view/ator/components/ator-list/ator-list.component.ts
+++ frontend/src/app/view/ator/components/ator-list/ator-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { LazyLoadEvent } from 'primeng/api';
 import { PageListEnum } from '../../../../shared/enums/page-list.enum';
 import { Page } from '../../../../shared/models/page.model';
+import { MensagemService } from '../../../../shared/services/mensagem.service';
 import { Ator } from '../../models/ator.model';
 import { AtorService } from '../../services/ator.service';
 
@@ -21,7 +22,8 @@ export class AtorListComponent {
 	viewAtorForm: boolean = false;
 
 	constructor(
-		private atorService: AtorService
+		private atorService: AtorService,
+		private mensagemService: MensagemService
 	) {
 	}
 
@@ -40,6 +42,12 @@ export class AtorListComponent {
 		return this.atoresSelecionados[0];
 	}
 
+	get mensagemExcluirAtores(): string {
+		return 'Tem certeza que seja excluir o(s) ator(es)/atriz(es)' +
+			this.atoresSelecionados.map(value => `<em>"${ value.nome }"</em>`).join(', ') +
+			'?';
+	}
+
 	buscarAtores(event?: LazyLoadEvent): void {
 		this.atoresSelecionados = [];
 		// TODO Implementar esse fluxo
@@ -55,12 +63,21 @@ export class AtorListComponent {
 	}
 
 	excluirAtores(): void {
-		// TODO Implementar esse fluxo
+		this.mensagemService.exibirMensagem(
+			'EXCLUIR ATOR(ES)',
+			this.mensagemExcluirAtores,
+			this,
+			() => this.excluir()
+		);
 	}
 
 	limparFiltro(): void {
 		this.filtro = new Ator();
 		this.buscarAtores();
+	}
+
+	private excluir(): void {
+		// TODO Implementar esse fluxo
 	}
 
 }
