4fd11e4f9e2451219bfcd101c8231c73c869de7e
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2023-01-28 13:55:52 -0300

    Criando componente para filtragem na lista de itens

---
+++ frontend/src/app/view/item/components/item-filter/item-filter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { DialogUtil } from '../../../../shared/utils/dialog.util';
+
+@Component({
+	selector: 'app-item-filter',
+	templateUrl: './item-filter.component.html',
+	styleUrls: ['./item-filter.component.scss']
+})
+export class ItemFilterComponent extends DialogUtil {
+
+}
